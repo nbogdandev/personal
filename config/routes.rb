a1e7a5949c9c0b6003cea88139d96026a63eadc8
nikitabogdan <nikita.bogdan@me.com>
2017-01-30 13:44:15 +1000

    add favicon, fix missing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-   get 'welcome/index'
-   root 'welcome#index'
-  
+  get 'welcome/index'
+  root 'welcome#index'
+  match '*path' => redirect('/'), via: :get
 end
